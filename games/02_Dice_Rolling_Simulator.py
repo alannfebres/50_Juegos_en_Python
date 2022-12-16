9caa60ca0f41a72cb3abb3af58566475297f0f9e
alannfebres <80296428+alannfebres@users.noreply.github.com>
2022-12-16 03:13:30 -0300

    Add files via upload

---
+++ games/02_Dice_Rolling_Simulator.py
@@ -0,0 +1,40 @@
+#SIMULADOR TIRAR EL DADO
+#Autor: Alann Febres (Wally)
+
+import random
+print("Simulador Arroja el Dado 🎲")
+enter =  "y"
+while enter == "y":
+    print("")
+    enter = input("Ingresa Y para arrojar el 🎲 - y N para terminar simulador: ")
+    print("----------------------------------------------")
+    print("")
+    if enter == "y":
+        dado = random.randint(1, 6)
+        print("🔄 🔄 🔄 🔄 🔄 🔄 🔄 🔄 🔄 ")
+        print("El resultado del dado es?:", dado)
+    else:   
+        print("🎲 🎲 🎲 🎲 🎲 🎲 🎲 🎲 🎲")
+        print("Se guarda el Dado... Hasta la Proxima 😉👍")
+        print("🎲 🎲 🎲 🎲 🎲 🎲 🎲 🎲 🎲")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
