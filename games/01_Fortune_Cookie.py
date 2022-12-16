fb87ff401086245034d3cc485750e609fcc7fcc0
alannfebres <80296428+alannfebres@users.noreply.github.com>
2022-12-16 03:12:39 -0300

    Add files via upload

--- games/01_Fortune_Cookie.py
+++ games/01_Fortune_Cookie.py
@@ -1,4 +1,6 @@
-#Galleta de la Fortuna
+# Galleta de la Fortuna
+# Autor: Alann Febres (Wally)
+
 import random
 print("")
 print("Pidele 3 consejo a la Galleta de la Fortuna 🥠")
@@ -6,36 +8,35 @@ print("----------------------------------------------")
 print("")
 for i in range(3):
     i += 1
-    #print("La galleta N°",i,"dice:")
+    # print("La galleta N°",i,"dice:")
     num = random.randint(1, 5)
     enter = input("Presiona enter para romper la galleta")
     print("")
     if num == 1:
         print("crunch🥠💥!")
-        print("=> La galleta N°",i,"dice:")
+        print("=> La galleta N°", i, "dice:")
         print("Hoy será un día maravilloso, aprovechalo!")
         print("")
     elif num == 2:
         print("crunch🥠💥!")
-        print("=> La galleta N°",i,"dice:")
+        print("=> La galleta N°", i, "dice:")
         print("Haz el bien y no mires a quien!")
         print("")
     elif num == 3:
         print("crunch🥠💥!")
-        print("=> La galleta N°",i,"dice:")
+        print("=> La galleta N°", i, "dice:")
         print("No mal gaste el dinero, invierte y traera frutos!")
         print("")
     elif num == 4:
         print("crunch🥠💥!")
-        print("=> La galleta N°",i,"dice:")
+        print("=> La galleta N°", i, "dice:")
         print("Trata de mantenerte quieto, no abuses de los excesos!")
         print("")
     else:
         print("crunch🥠💥!")
-        print("=> La galleta N°",i,"dice:")
+        print("=> La galleta N°", i, "dice:")
         print("Descansa!, a sido un largo trayecto")
         print("")
 print("*********************************")
 print("Adios y buena suerte en todo!!!")
 print("*********************************")
-
